e72e2f0da76953d22dc62f37e622b915b5784323
EC2 Default User <ec2-user@ip-172-31-71-116.ec2.internal>
2021-09-18 10:17:07 +0000

    completed cart_item function

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -11,19 +11,22 @@ class Public::CartItemsController < ApplicationController
     # →ずっとこの書き方を試していて、何が違うのかがわからなかったが、原因はhidden_fieldの書き方が誤っていたことだったので、この記述で問題なかった
     # @item = Item.find(params[:cart_item][:item_id])
 
-      # (item_id: params[:cart_item][:item_id])この書き方を覚える　→item_idはcart_itemモデルの中のitem_idであるということ
-      # なぜかこの部分でエラーが出る
-      @cart_item = current_customer.cart_items.find_by(params[:cart_item][:item_id])
-      if @cart_item.present?
-        @cart_item.amount += @cart_item(params[:cart_item][:amount]).to_i
-        @cart_item.save
-      else
-        @cart_item = CartItem.new(cart_item_params)
-        @item = Item.find(params[:cart_item][:item_id])
-        @cart_item.customer = current_customer
-        @cart_item.item_id = @item.id
-        @cart_item.save
-      end
+    # (item_id: params[:cart_item][:item_id])この書き方を覚える　→item_idはcart_itemモデルの中のitem_idであるということ、これがないと、item_idで検索をかけられず、一番上のidを持つデータが更新される
+    @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+    # なぜかこの部分でエラーが出る　→次の日にやってみたらうまく行ったので、rails sをし直さなかったからかも
+    if @cart_item.present?
+      #params[:amount]のみだったらどこのテーブルを参照できるかわからないのでエラーが出る
+      #.to-iをつけないと、paramsのデフォルト型であるstring型でデータが渡され、String can't be coerced into Integerというエラーが出る
+      @cart_item.amount += params[:cart_item][:amount].to_i
+      #update(cart_item_params)としてしまうと、今回新たに入力したamountのみがparameterとして渡り、上のコマンドが有効にならないので、下記のように変更する
+      @cart_item.update(amount: @cart_item.amount)
+    else
+      @cart_item = CartItem.new(cart_item_params)
+      @item = Item.find(params[:cart_item][:item_id])
+      @cart_item.customer = current_customer
+      @cart_item.item_id = @item.id
+      @cart_item.save
+    end
       redirect_to cart_items_path
 
   end
